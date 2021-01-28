dbe76118450aa20e0fca80e85d36b0258fb106b1
Sugar_F0x <sugarfox.dev@gmail.com>
2021-01-28 17:44:09 +0300

    :white_check_mark: Add Rating component
    > Display 0-5 star rating

---
+++ components/Rating.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div.pt-3
+    v-icon(
+      v-for="n in 5"
+      :key="'rate-'+n"
+    ) {{ getStar(n) }}
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "Rating",
+  props: {
+    rating: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    getStar(n: number) {
+      if (this.rating >= n) {
+        return 'mdi-star'
+      } else if (this.rating - n > 0) {
+        return 'mdi-star-half-full'
+      } else {
+        return 'mdi-star-outline'
+      }
+    }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
