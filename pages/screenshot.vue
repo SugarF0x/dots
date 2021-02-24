9d49f61e8b95d4a447423ba45c270a013daf0554
Sugar_F0x <sugarfox.dev@gmail.com>
2021-02-24 23:57:29 +0300

    :construction: Add name crop
    > Crop out images with names & heroes of both teams' players

--- pages/screenshot.vue
+++ pages/screenshot.vue
@@ -1,13 +1,28 @@
 <template lang="pug">
   v-container
     v-row(justify="center" align="center")
-      v-stage(:config="konva" id="konva")
+      v-stage(:config="konva" ref="stage").d-none
         v-layer
           v-image(:config="img")
+      v-stage(:config="snippet")
+        v-layer
+          v-image(
+            v-for="n in 5"
+            :key="'ass'+n"
+            :config="getImageConfig(n, 'ally')"
+          )
+      v-stage(:config="snippet")
+        v-layer
+          v-image(
+            v-for="n in 5"
+            :key="'ass'+n"
+            :config="getImageConfig(n, 'enemy')"
+          )
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Konva from 'konva'
 
 export default Vue.extend({
   name: "screenshot",
@@ -20,30 +35,91 @@ export default Vue.extend({
         scaleX: 1,
         scaleY: 1,
       },
+      snippet: {
+        width: 125,
+        height: 250,
+        scaleX: 1,
+        scaleY: 1,
+      },
       img: {
         x: 0,
         y: 0,
         image: null as any,
         width: 1280,
         height: 720,
-        rotation: 0,
+        rotation: -30,
         crop: {
           x: 0,
           y: 0,
           width: 1280,
           height: 720
         }
-      } as any
+      } as any,
+      allyImage: null as null | HTMLImageElement,
+      enemyImage: null as null | HTMLImageElement,
+    }
+  },
+  methods: {
+    getImageConfig(n: number, type: 'ally' | 'enemy') {
+      const coordsAlly = [
+        {x:50,y:80},
+        {x:162,y:145},
+        {x:162,y:275},
+        {x:275,y:340},
+        {x:275,y:470}
+      ]
+      const coordsEnemy = [
+        {x:930,y:220},
+        {x:820,y:285},
+        {x:820,y:415},
+        {x:710,y:482},
+        {x:710,y:610}
+      ]
+
+      return {
+        x: 0,
+        y: (n-1)*50,
+        image: type === 'ally' ? this.allyImage : this.enemyImage,
+        width: 125,
+        height: 40,
+        crop: {
+          ...(type === 'ally' ? coordsAlly[n-1] : coordsEnemy[n-1]),
+          width: 125,
+          height: 40
+        }
+      }
     }
   },
   created() {
     const source = new Image()
     source.src = require('~/assets/dummyScreenshot.jpeg')
     source.onload = () => {
+      source.onload = null
       const img = this.img
+      const stage = (this.$refs.stage as any).getNode() as Konva.Node
       img.image = source
 
-      img.rotation = -30
+      const allyImage = new Image()
+      this.$nextTick(() => {
+        allyImage.src = stage.toDataURL()
+        allyImage.onload = () => {
+          allyImage.onload = null
+          this.allyImage = allyImage
+        }
+
+        img.rotation = 30
+        img.y = -500
+        this.$nextTick(() => {
+          const enemyImage = new Image()
+          enemyImage.src = stage.toDataURL()
+          enemyImage.onload = () => {
+            enemyImage.onload = null
+            this.enemyImage = enemyImage
+            img.rotation = 0
+            img.y = 0
+          }
+        })
+      })
     }
   }
 })
