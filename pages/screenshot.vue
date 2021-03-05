b1c0b1537a3f0413713406e3630f0ff66a8f06f7
Sugar_F0x <sugarfox.dev@gmail.com>
2021-03-05 13:26:29 +0300

    :recycle: Code cleanup
    Refactor caps const names into unified objects

--- pages/screenshot.vue
+++ pages/screenshot.vue
@@ -77,29 +77,31 @@ export default Vue.extend({
     }
   },
   async mounted() {
-    // TODO: refactor constants to objects e.g. MAIN_STAGE & CROP_STAGE are to be stage.main & stage.crop
-
     /**
      * Define Main & Crop Konva Stages & working Layer
      */
-    const MAIN_STAGE = new Konva.Stage({
-      container: 'mainStage',
-      width: 1920,
-      height: 1080
-    })
-    const CROP_STAGE = new Konva.Stage({
-      container: 'cropStage',
-      width: 140,
-      height: 20
-    })
+    const stage = {
+      main: new Konva.Stage({
+        container: 'mainStage',
+        width: 1920,
+        height: 1080
+      }),
+      crop: new Konva.Stage({
+        container: 'cropStage',
+        width: 140,
+        height: 20
+      })
+    }
 
     /**
      * Define working Layers
      */
-    const MAIN_LAYER = new Konva.Layer()
-    MAIN_STAGE.add(MAIN_LAYER)
-    const CROP_LAYER = new Konva.Layer()
-    CROP_STAGE.add(CROP_LAYER)
+    const layer = {
+      main: new Konva.Layer(),
+      crop: new Konva.Layer()
+    }
+    stage.main.add(layer.main)
+    stage.crop.add(layer.crop)
 
     /**
      * Get source image that will be used in every new Image instance
@@ -110,76 +112,84 @@ export default Vue.extend({
     /**
      * Define Konva Image that will be used in transformations
      */
-    const IMAGE = new Konva.Image({
+    const image = new Konva.Image({
       x: 0,
       y: 0,
       image: SOURCE_IMAGE,
       width: 1920,
       height: 1080
     })
-    MAIN_LAYER.add(IMAGE)
+    layer.main.add(image)
 
     /**
      * Create Ally & Enemy Image source instances
      */
-    IMAGE.rotation(-30)
-    const ALLY_SOURCE_IMAGE = await this.getImage(MAIN_STAGE.toDataURL())
-    IMAGE.rotation(30)
-    IMAGE.y(-750)
-    const ENEMY_SOURCE_IMAGE = await this.getImage(MAIN_STAGE.toDataURL())
-    IMAGE.rotation(0)
-    IMAGE.y(0)
+    image.rotation(-30)
+    const ALLY_SOURCE_IMAGE = await this.getImage(stage.main.toDataURL())
+    image.rotation(30)
+    image.y(-750)
+    const ENEMY_SOURCE_IMAGE = await this.getImage(stage.main.toDataURL())
+    image.rotation(0)
+    image.y(0)
 
     /**
      * Create arrays of single names for both teams
      */
-    const ALLY_NAMES = [] as HTMLImageElement[]
-    const ENEMY_NAMES = [] as HTMLImageElement[]
+    const names = {
+      ally: [] as HTMLImageElement[],
+      enemy: [] as HTMLImageElement[]
+    }
     for (let i=0; i<5; i++) {
-      const CROP_ALLY_IMAGE = new Konva.Image({
-        ...this.getCropConfig(ALLY_SOURCE_IMAGE, 'ally', i)
-      })
-      const CROP_ENEMY_IMAGE = new Konva.Image({
-        ...this.getCropConfig(ENEMY_SOURCE_IMAGE, 'enemy', i)
-      })
+      const crop = {
+        ally: new Konva.Image({
+          ...this.getCropConfig(ALLY_SOURCE_IMAGE, 'ally', i)
+        }),
+        enemy: new Konva.Image({
+          ...this.getCropConfig(ENEMY_SOURCE_IMAGE, 'enemy', i)
+        })
+      }
 
-      CROP_LAYER.add(CROP_ALLY_IMAGE)
-      ALLY_NAMES.push(await this.getImage(CROP_STAGE.toDataURL()))
-      CROP_ALLY_IMAGE.destroy()
+      layer.crop.add(crop.ally)
+      names.ally.push(await this.getImage(stage.crop.toDataURL()))
+      crop.ally.destroy()
 
-      CROP_LAYER.add(CROP_ENEMY_IMAGE)
-      ENEMY_NAMES.push(await this.getImage(CROP_STAGE.toDataURL()))
-      CROP_ENEMY_IMAGE.destroy()
+      layer.crop.add(crop.enemy)
+      names.enemy.push(await this.getImage(stage.crop.toDataURL()))
+      crop.enemy.destroy()
     }
 
     /**
      * Create arrays of names with filters applied
      */
-    const FILTERED_ALLY_NAMES = [] as HTMLImageElement[]
-    const FILTERED_ENEMY_NAMES = [] as HTMLImageElement[]
+    const filteredNames = {
+      ally: [] as HTMLImageElement[],
+      enemy: [] as HTMLImageElement[]
+    }
     for (let i=0; i<5; i++) {
-      const FILTERED_ALLY_IMAGE = new Konva.Image({
-        x: 0,
-        y: 0,
-        image: ALLY_NAMES[i],
-        filters: [Konva.Filters.Contrast, Konva.Filters.Grayscale, Konva.Filters.Invert]
-      })
-      const FILTERED_ENEMY_IMAGE = new Konva.Image({
-        x: 0,
-        y: 0,
-        image: ENEMY_NAMES[i],
-        filters: [Konva.Filters.Contrast, Konva.Filters.Grayscale, Konva.Filters.Invert]
-      })
+      const filtered = {
+        ally: new Konva.Image({
+          x: 0,
+          y: 0,
+          image: names.ally[i],
+          filters: [Konva.Filters.Contrast, Konva.Filters.Grayscale, Konva.Filters.Invert]
+        }),
+        enemy: new Konva.Image({
+          x: 0,
+          y: 0,
+          image: names.enemy[i],
+          filters: [Konva.Filters.Contrast, Konva.Filters.Grayscale, Konva.Filters.Invert]
+        })
+      }
 
-      FILTERED_ALLY_IMAGE.cache()
-      CROP_LAYER.add(FILTERED_ALLY_IMAGE)
-      FILTERED_ALLY_NAMES.push(await this.getImage(CROP_LAYER.toDataURL()))
-      FILTERED_ALLY_IMAGE.destroy()
+      filtered.ally.cache()
+      layer.crop.add(filtered.ally)
+      filteredNames.ally.push(await this.getImage(layer.crop.toDataURL()))
+      filtered.ally.destroy()
 
-      FILTERED_ENEMY_IMAGE.cache()
-      CROP_LAYER.add(FILTERED_ENEMY_IMAGE)
-      FILTERED_ENEMY_NAMES.push(await this.getImage(CROP_LAYER.toDataURL()))
-      FILTERED_ENEMY_IMAGE.destroy()
+      filtered.enemy.cache()
+      layer.crop.add(filtered.enemy)
+      filteredNames.enemy.push(await this.getImage(layer.crop.toDataURL()))
+      filtered.enemy.destroy()
     }
   }
 })
